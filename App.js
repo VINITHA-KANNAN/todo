5f39812ac56642161168d78d7c937e791d0383f5
VINITHA-KANNAN <96303716+VINITHA-KANNAN@users.noreply.github.com>
2022-02-20 15:38:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoList from './Component/TodoList';
+
+ function App() {
+  return (
+    <div className="TodoListcontainer">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+export default App;
